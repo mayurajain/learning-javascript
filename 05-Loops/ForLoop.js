594ae9a7d807ece11d2724b9481c8c3d01d75130
mayurajain <mayurjainkm@gmail.com>
2022-05-31 22:14:10 +0530

    Updated examples for For and Foreach loops

---
+++ 05-Loops/ForLoop.js
@@ -0,0 +1,6 @@
+// SECTION - For is kind of same as in other language
+
+let days=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+for(let i=0;i<=days.length-1;i++){
+    console.log(days[i]);
+}
--- 05-Loops/ForeachLoop.js
+++ 05-Loops/ForeachLoop.js
@@ -1,1 +1,7 @@
 // SECTION - Foreach is kind of same as in other language
+
+let days=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+days.forEach(function(day,index){
+    console.log((index+1)+":"+day)
+})
