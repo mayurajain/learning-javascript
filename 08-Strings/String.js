840456b7fc121abc9f5f20896af4bdd678dc564e
mayurajain <mayurjainkm@gmail.com>
2022-06-02 23:21:49 +0530

    Added links to learn about strings in string.js

--- 08-Strings/String.js
+++ 08-Strings/String.js
@@ -1,1 +1,4 @@
 // SECTION - Strings Section
+
+// TODO - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+// Refer methods in this link to learn more about strings in javascript
