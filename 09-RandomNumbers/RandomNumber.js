9467044dc26c50549e0f21cb736f0ced1f6baf3b
mayurajain <mayurjainkm@gmail.com>
2022-06-08 22:48:08 +0530

    Examples and references for Random number generation

--- 09-RandomNumbers/RandomNumber.js
+++ 09-RandomNumbers/RandomNumber.js
@@ -1,1 +1,22 @@
 // SECTION - Random Number generation in JavaScript
+
+// to get decimal number between 0 to 9.99
+let randomNumber=Math.random()*10;
+console.log(randomNumber);
+
+// to get int number between 0 to 9
+let randomNumber2=Math.random()*10;
+console.log(Math.floor(randomNumber2));
+
+// To get a int number between 0 to 10
+let randomNumber3=Math.random()*10+1;
+console.log(Math.floor(randomNumber3));
+
+// TODO - Find more about random numbers in javascript from mdn documentation
+
+let number=3.53636252525;
+console.log(number.toFixed(4))
+
+// TODO - Find more about numbers and methods on it in javascript from mdn documentation
+
+
